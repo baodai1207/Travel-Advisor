a85c5814d87522a2c44fb2400f9f3afc098ad295
Dai Huynh <dai.huynh@ucdenver.edu>
2021-08-20 16:40:41 -0600

    Integrated Google Map API

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-const List =()=>{
-  return (
-    <h1> List</h1>
-  )
-}
+import React from 'react';
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLable,
+  MenyItem,
+  FormControl,
+  Select,
+} from '@material-ui/core';
+
+import useStyles from './styles';
+
+const List = () => {
+  return <h1> List</h1>;
+};
 export default List;
